272c77076b8d5509d29e425d217a5b9042ea8a84
Kim Fransman <kim.fransman@gmail.com>
2015-09-30 04:38:00 +0200

    moved propTypes to a static property on ListItem

--- src/views/ListItem.js
+++ src/views/ListItem.js
@@ -1,6 +1,11 @@
 import React, { Component, PropTypes } from 'react';
 
 export default class ListItem extends Component {
+  static propTypes = {
+    onClick: PropTypes.func,
+    title: PropTypes.string.isRequired
+  }
+
   render() {
     return (
       <li onClick={this.props.onClick}>
@@ -9,8 +14,3 @@ export default class ListItem extends Component {
     );
   }
 }
-
-ListItem.propTypes = {
-  onClick: PropTypes.func,
-  title: PropTypes.string.isRequired
-};
